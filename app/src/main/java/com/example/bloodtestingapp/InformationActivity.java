3288c26e099367291180f0571d0e6a78ad58dfd5
Anurag Sharma <sharmaanurag1910@gmail.com>
2021-06-17 16:19:04 +0530

    Information Button

---
+++ app/src/main/java/com/example/bloodtestingapp/InformationActivity.java
@@ -0,0 +1,19 @@
+package com.example.bloodtestingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+
+        getSupportActionBar().hide();
+
+
+
+    }
+}
