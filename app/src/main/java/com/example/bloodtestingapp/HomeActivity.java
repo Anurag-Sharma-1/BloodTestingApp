0536871a029eed9cdd598b3a0ea900e51acfc030
Anurag Sharma <sharmaanurag1910@gmail.com>
2021-06-17 17:43:01 +0530

    InformationActivity

--- app/src/main/java/com/example/bloodtestingapp/HomeActivity.java
+++ app/src/main/java/com/example/bloodtestingapp/HomeActivity.java
@@ -2,10 +2,14 @@ package com.example.bloodtestingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +18,14 @@ public class HomeActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
-        
+        imageButton = findViewById(R.id.imageButton3);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this,InformationActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
